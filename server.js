5a21af8f18b30a4a34eb95d176a9c63d76e653bf
Yarosalv Morozov <yymoroz3@gmail.com>
2016-11-13 21:40:07 +0300

    AllTest

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const config = require('./config');
-
-//const fetch = require('node-fetch');
-
+const fetch = require('node-fetch'); 
 const app = express();
 const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-const pc = {
+let pc = {
     "board": {
         "vendor": "IBM",
         "model": "IBM-PC S-100",
@@ -42,6 +39,21 @@ const pc = {
     "width": 54
 };
 
+fetch(pcUrl)
+    .then((res) => {
+        console.log('Start get data');
+        return res.json();
+    })
+    .then((json) => {
+        pc = json;
+        app.listen(3000, ()=>{
+            console.log('Success! '+3000+' port run');
+        });
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -52,16 +64,19 @@ app.use((req, res, next) => {
 
 
 function getObj(obj, pathArr){
-    debugger;
     let data=obj, flagExit = false;
     pathArr.forEach((el) => {
+        debugger;
         if(!el) {
            flagExit = true; 
         }
         else{
             if(!flagExit){
+                if(data.constructor()[el] !== undefined){
+                    data = void(0);
+                }
                 data = data[el];
-                if(!data == void(0)) flagExit = true;
+                if(data == void(0)) flagExit = true;
             }
         }
     })
@@ -72,49 +87,25 @@ function getObj(obj, pathArr){
 
 app.get('/volumes', (req, res)=>{
     let data = {};
-    debugger;
-    console.log('Req url: ',  req.url);
     pc.hdd.forEach((el) => {
         data[el.volume] = data[el.volume]?data[el.volume]+el.size:el.size;
     })
     for(let el in data){
         data[el] += 'B'
     }
-    console.log('Res volumes data: ' + data + '\n------------------------------------')
     res.status(200).send(JSON.stringify(data));
 });
 
 app.get(/[\w\/]*/, (req, res) => {
     let patch = (req.url.slice(1)).split('/');
     console.log('Req url: ',  req.url);
-    debugger;
     let result = getObj(pc, patch);
-    console.log('Res data: ' + result + '\n------------------------------------')
-    if(result== void(0)) throw new Error('No data');
+    console.log('Res data: ' + result + '\n------------------------------------');
+    debugger;
+    if(typeof result == "undefined") throw new Error('No data');
     else res.send(JSON.stringify(result));
 });
 
 app.use((err, req, res, next) => {   
     res.status(404).send('Not Found');
 })
-
-
-
-
-app.listen(3000, ()=>{
-            console.log('Success! '+3000+' port run');
-        });
-/*
-fetch(pcUrl)
-    .then((res) => {
-        return res.json();
-    })
-    .then((pcJson) => {
-        pc = pcJson;
-        app.listen(3000, ()=>{
-            console.log('Success! '+config.port+' port run');
-        });
-    })
-    .catch((error) => {
-        console.log('Error!', error);
-    });*/
