3187decddc44af54c026f17da39ac59f987c5c8b
neeeekto <iceas@mail.ru>
2016-11-14 12:52:47 +0300

    refact

--- server.js
+++ server.js
@@ -2,42 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch'); 
 const app = express();
 const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-let pc = {
-    "board": {
-        "vendor": "IBM",
-        "model": "IBM-PC S-100",
-        "cpu": {
-            "model": "80286",
-            "hz": 12000
-        },
-        "image": "http://www.s100computers.com/My%20System%20Pages/80286%20Board/Picture%20of%2080286%20V2%20BoardJPG.jpg",
-        "video": "http://www.s100computers.com/My%20System%20Pages/80286%20Board/80286-Demo3.mp4"
-    },
-    "ram": {
-        "vendor": "CTS",
-        "volume": 1048576,
-        "pins": 30
-    },
-    "os": "MS-DOS 1.25",
-    "floppy": 0,
-    "hdd": [{
-        "vendor": "Samsung",
-        "size": 33554432,
-        "volume": "C:"
-    }, {
-        "vendor": "Maxtor",
-        "size": 16777216,
-        "volume": "D:"
-    }, {
-        "vendor": "Maxtor",
-        "size": 8388608,
-        "volume": "C:"
-    }],
-    "monitor": null,
-    "length": 42,
-    "height": 21,
-    "width": 54
-};
+let pc = {};
 
 fetch(pcUrl)
     .then((res) => {
@@ -98,10 +63,7 @@ app.get('/volumes', (req, res)=>{
 
 app.get(/[\w\/]*/, (req, res) => {
     let patch = (req.url.slice(1)).split('/');
-    console.log('Req url: ',  req.url);
     let result = getObj(pc, patch);
-    console.log('Res data: ' + result + '\n------------------------------------');
-    debugger;
     if(typeof result == "undefined") throw new Error('No data');
     else res.send(JSON.stringify(result));
 });
